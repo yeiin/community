b2dfa77dd7fe7011d7960c15dcdd0cb5048501d3
yeiin <88276674+yeiin@users.noreply.github.com>
2025-10-01 13:13:57 +0900

    :art: Add Member, Auth, Post, Comment entities

---
+++ src/main/java/com/ktb/community/domain/auth/Auth.java
@@ -0,0 +1,24 @@
+package com.ktb.community.domain.auth;
+
+import com.ktb.community.domain.BaseTime;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "auth")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Auth extends BaseTime {
+
+    @Id
+    @MapsId
+    @Column(name = "member_id")
+    private long memberId;
+
+    private String refreshTokenHash;
+
+    public Auth(long memberId, String refreshTokenHash) {
+        this.memberId = memberId;
+        this.refreshTokenHash = refreshTokenHash;
+    }
+}
---
+++ src/main/java/com/ktb/community/domain/post/Post.java
@@ -0,0 +1,33 @@
+package com.ktb.community.domain.post;
+
+import com.ktb.community.domain.BaseTime;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTime {
+
+    @Id
+    @Column(name = "post_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long memberId;
+
+    private String title;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String contents;
+
+    private String imageUrl;
+
+    public Post( long memberId, String title, String contents, String imageUrl) {
+        this.memberId = memberId;
+        this.title = title;
+        this.contents = contents;
+        this.imageUrl = imageUrl;
+    }
+}
