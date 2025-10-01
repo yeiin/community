25ccd534e3c8e28fb90dd240134d3289ecd0027e
yeiin <88276674+yeiin@users.noreply.github.com>
2025-10-01 11:25:54 +0900

    :tada: start community service

---
+++ src/test/java/com/ktb/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
