3990c380514ed334de07d6bd350ecda1ed71465b
yeiin <88276674+yeiin@users.noreply.github.com>
2025-10-28 18:25:09 +0900

    :sparkles: number formatter 구현

--- src/main/java/com/ktb/community/dto/post/response/PostCounterResponse.java
+++ src/main/java/com/ktb/community/dto/post/response/PostCounterResponse.java
@@ -1,20 +1,22 @@
 package com.ktb.community.dto.post.response;
 
 import com.ktb.community.domain.post_stats.PostStats;
+import com.ktb.community.global.processor.NumberProcessor;
 import lombok.Builder;
 
 @Builder
 public record PostCounterResponse(
-        long likes,
-        long comments,
-        long views
+        String likes,
+        String comments,
+        String views
 ) {
 
     public static PostCounterResponse from(PostStats postStats) {
         return PostCounterResponse.builder()
-                .likes(postStats.getLikeCount())
-                .comments(postStats.getCommentCount())
-                .views(postStats.getViewCount())
+                .likes(NumberProcessor.getFormatNumber(postStats.getLikeCount()))
+                .comments(NumberProcessor.getFormatNumber(postStats.getCommentCount()))
+                .views(NumberProcessor.getFormatNumber(postStats.getViewCount()))
                 .build();
     }
+
 }
